f4ab9d8f4866880dbe18bd7934b42c2972ba4a4f
oddfar <oddfar@163.com>
2024-02-02 23:02:27 +0800

    update: REAMME

--- src/main/java/com/oddfar/ai/discord/CozeDiscordApplication.java
+++ src/main/java/com/oddfar/ai/discord/CozeDiscordApplication.java
@@ -3,11 +3,17 @@ package com.oddfar.ai.discord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 使用两个Discord机器人代理Coze服务，免费使用GPT-4高级模型的API功能
+ * GitHub: https://github.com/oddfar/coze-discord
+ *
+ * @author oddfar
+ */
 @SpringBootApplication
 public class CozeDiscordApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CozeDiscordApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CozeDiscordApplication.class, args);
+    }
 
 }
