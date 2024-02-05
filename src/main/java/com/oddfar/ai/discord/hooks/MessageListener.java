980e9cf8d6bfa29f836f1e75c6a472090a334d47
致远 <oddfar@163.com>
2024-02-05 23:22:57 +0800

    Feature 对话支持文生图 (#3)

--- src/main/java/com/oddfar/ai/discord/hooks/MessageListener.java
+++ src/main/java/com/oddfar/ai/discord/hooks/MessageListener.java
@@ -1,18 +1,22 @@
 package com.oddfar.ai.discord.hooks;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.extra.spring.SpringUtil;
 import com.oddfar.ai.discord.config.properties.DiscordProperties;
 import com.oddfar.ai.discord.domain.resp.ChatResp;
 import com.oddfar.ai.discord.manage.MessageFuturesManager;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.MessageType;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 /**
  * 消息监听
@@ -41,10 +45,19 @@ public class MessageListener extends ListenerAdapter {
         //引用的消息
         Message referencedMessage = message.getReferencedMessage();
         if (referencedMessage == null) return;
-        String content = message.getContentRaw();
-        log.info("收到消息：{}", content);
+        List<MessageEmbed> embeds = message.getEmbeds();
+        //嵌套消息不为空则为图片
+        if (CollUtil.isNotEmpty(embeds)) {
+            setFuturesEmbed(referencedMessage, message);
+        } else {
+            String content = message.getContentRaw();
+            log.info("收到消息：{}", content);
+        }
+
         log.info("收到消息的引用消息Id：{}", referencedMessage.getId());
+
     }
+
 
     /**
      * 消息更新时的回调
@@ -65,6 +78,12 @@ public class MessageListener extends ListenerAdapter {
         //引用的消息
         Message referencedMessage = message.getReferencedMessage();
         boolean stream = MessageFuturesManager.streamMessageList.contains(referencedMessage.getIdLong());
+        //嵌套消息不为空则为图片
+        List<MessageEmbed> embeds = message.getEmbeds();
+        if (CollUtil.isNotEmpty(embeds)) {
+            setFuturesEmbed(referencedMessage, message);
+            return;
+        }
         //是否存在按钮 = 100%响应完毕，只返回一次
         boolean component = message.getComponents().isEmpty();
         if (stream && component) {
@@ -88,6 +107,22 @@ public class MessageListener extends ListenerAdapter {
         if (future != null && !future.isDone()) {
             //构建ChatResp信息
             ChatResp chatResp = new ChatResp(message, referencedMessage, done);
+            future.complete(chatResp);
+        }
+    }
+
+    private void setFuturesEmbed(Message referencedMessage, Message message) {
+        CompletableFuture<ChatResp> future = MessageFuturesManager.getFutures(referencedMessage.getIdLong());
+        if (future != null && !future.isDone()) {
+            //构建ChatResp的嵌套信息
+            ChatResp chatResp = new ChatResp(message, referencedMessage, true);
+            List<MessageEmbed> embeds = message.getEmbeds();
+            //获取图片列表
+            List<String> images = embeds.stream().map(e -> e.getImage().getUrl()).collect(Collectors.toList());
+            chatResp.setEmbedImageUrls(images);
+            log.info("嵌套消息图片Urls {}", images);
+
+
             future.complete(chatResp);
         }
     }
